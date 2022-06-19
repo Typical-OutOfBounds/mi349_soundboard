9bb06d7ebc8c4e4dde9fae978abd08f9557870b6
Aidan Lane <aidanlane0@protonmail.com>
2022-06-19 14:57:47 -0400

    added buttons and sounds

---
+++ soundboard.js
@@ -0,0 +1,41 @@
+function gameSoundOne() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'game1.wav')
+
+    audio.play();
+}
+
+function gameSoundTwo() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'game2.wav')
+
+    audio.play();
+}
+
+function classicalSoundOne() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'classical1.wav')
+
+    audio.play();
+}
+
+function classicalSoundTwo() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'classical2.mp3')
+
+    audio.play();
+}
+
+function outdoorsSoundOne() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'outdoors1.wav')
+
+    audio.play();
+}
+
+function outdoorsSoundTwo() {
+    var audio = document.getElementById("audio")
+    audio.setAttribute("src", 'outdoors2.mp3')
+
+    audio.play();
+}
